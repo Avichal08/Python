8aa01b0a7ac2a40d6ea3cdd3c86017f77f92472c
Avichal08 <61931349+Avichal08@users.noreply.github.com>
2020-09-21 21:47:59 +0530

    Add files via upload

---
+++ Fibonacci_R.py
@@ -0,0 +1,8 @@
+n = int(input("Enter a number : "))
+def fibonacci(n):
+    if n == 0 or n == 1 :
+        return n
+    else :
+        return fibonacci(n-2) + fibonacci(n-1)
+
+print(fibonacci(n))        
