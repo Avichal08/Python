b31ce7f2c8413cb5657fc8df2e683b6205a1e972
Avichal08 <61931349+Avichal08@users.noreply.github.com>
2020-10-20 19:23:49 +0530

    Add files via upload

---
+++ RevArray.py
@@ -0,0 +1,12 @@
+arr = []
+size = int(input("Enter no Elements in array  "))
+for n in range(size):
+    num = int(input("Enter Elements  "))
+    arr.append(num)
+
+
+print(arr)
+arr.reverse()
+print("reversed list is") 
+print(arr)       
+
